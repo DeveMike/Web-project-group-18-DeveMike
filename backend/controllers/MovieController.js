32ca714129d0a6ec3fb6c7da8312a85a68a9f8f6
Thomas Stremmelaar <t.stremmie@gmail.com>
2025-09-17 16:14:25 +0300

    can add movies to database

--- backend/controllers/MovieController.js
+++ backend/controllers/MovieController.js
@@ -5,9 +5,10 @@ const { Readable } = require('stream')
 const pool = require('../config/database.js')
 
 class MovieController {
+  #imgBaseUrl = undefined
+
   constructor() {
     this.apiUrl = 'https://api.themoviedb.org/3/'
-
     this.options = {
       method: 'GET',
       headers: {
@@ -15,87 +16,115 @@ class MovieController {
         Authorization: 'Bearer '+process.env.TMDB_TOKEN
       }
     }
-  }  
+  }
   
   async #getImgBaseUrl() {
-    // get base url required for fetching images from tmdb
-    const response = await fetch('https://api.themoviedb.org/3/configuration', this.options)
-    const data = await response.json()
-    if(response.status === 200) {
-      this.imgBaseUrl = data.images.base_url
-    } else {
-      throw Error("Can't fetch config from TMDB API")
+    if(!this.#imgBaseUrl) {
+      // get base url required for fetching images from tmdb
+      try {
+        const response = await fetch('https://api.themoviedb.org/3/configuration', this.options)
+        const data = await response.json()
+        if(response.status === 200) {
+          const imgBaseUrl = data.images.base_url
+          this.#imgBaseUrl = imgBaseUrl
+          return imgBaseUrl
+        } else {
+          return this.#imgBaseUrl
+        }
+      } catch(err) {
+        return null
+      }
     }
   }
 
-  async #checkIfInDb() {
+  async #dbSelect(id) {
     // check if movie exists in database
     try {
       const result = await pool.query('SELECT * FROM movies WHERE tmdb_id=$1', [id])
-      const raw = result.rows[0]
-      this.movieData = {
-        id: raw.tmdb_id,
-        title: raw.title,
-        overview: raw.description,
-        poster_url: raw.poster_url,
-        release_year: raw.release_year,
-        genre: raw.genre,
-        vote_average: raw.tmdb_rating
+      if(result.rows.length === 0) {
+        console.log("doesn't exist")
+        return undefined
+      } else {
+        console.log("exists")
+        const raw = result.rows[0]
+        const response = {
+          id: parseInt(raw.tmdb_id, 10),
+          title: raw.title,
+          overview: raw.description,
+          poster_url: raw.poster_url,
+          release_year: raw.release_year,
+          genre: raw.genre,
+          vote_average: raw.tmdb_rating
+        }
+        return response
       }
-      this.dbHasMovie = (result.rows.length === 0) ? false : true
     } catch(err) {
       return err
     }
   }
 
   async getMovie(req, res, next) {
-    if(!this.imgBaseUrl) {
+    const id = req.params.id.toString()
+
+    // get movie from tmdb if it isn't in the database
+    const fetchMovie = async () => {
       try {
-        this.#getImgBaseUrl()
+        const movieUrl = this.apiUrl+'movie/'+id+'?language=fi-FI'
+        const response = await fetch(movieUrl, this.options)
+        const data = await response.json()
+        if(response.status === 200) {
+          return data
+        } else {
+          return Error("Couldn't fetch movie")
+        }
       } catch(err) {
-        return next(err)
+        return err
       }
     }
 
-    this.#checkIfInDb()
-    
-    if(this.dbHasMovie) {
-      return res.status(200).json(this.movieData)
-    }
-
-    const id = req.params.id
-    const movieUrl = this.apiUrl+'movie/'+id+'?language=fi-FI'
-    let data = {}
-    let movieFetchStatus = -1
-    // get movie from tmdb if it isn't in the database
-    try {
-      const response = await fetch(movieUrl, this.options)
-      data = await response.json()
-      movieFetchStatus = response.status
-    } catch(err) {
-      return next(err)
-    }
-
     // add movie to database
-    let queryResult = {}
+    const insertMovieOk = async () => {
+      try {
+        const data = await fetchMovie()
+        const imgBaseUrl = await this.#getImgBaseUrl()
+        const result = await pool.query(
+          'INSERT INTO movies (tmdb_id, title, description, poster_url, release_year, genre, tmdb_rating)'
+          +'VALUES ($1, $2, $3, $4, $5, $6, $7)',
+          [
+            data.id, data.title, data.overview, imgBaseUrl+'w185'+data.poster_path,
+            parseInt(data.release_date.slice(0, 4)), data.genres[0].name, data.vote_average, 
+          ]
+        )
+        if(result.rowCount === 1) {
+          return true
+        } else {
+          return Error("Couldn't add movie to database")
+        }
+      } catch(err) {
+        return err
+      }
+    }
+
     try {
-      const queryResult = await pool.query(
-        'INSERT INTO movies (tmdb_id, title, description, poster_url, release_year, genre, tmdb_rating)'
-        +'VALUES ($1, $2, $3, $4, $5, $6, $7)',
-        [
-          apiData.id, apiData.title, apiData.overview, this.imgBaseUrl+'w185'+apiData.poster_path,
-          parseInt(apiData.release_date.slice(0, 4)), apiData.genres[0].name, apiData.vote_average, 
-        ]
-      )
+      const dbData = await this.#dbSelect(id)
+      console.log(dbData)
+      if(dbData) {
+        console.log("sending data to browser")
+        return res.status(200).json(dbData)
+      }
+      if(!dbData) {
+        if(await insertMovieOk()) {
+          const dbDataAfterInsert = await this.#dbSelect(id)
+          if(!dbDataAfterInsert) {
+            throw Error("Couldn't add movie to database")
+          } else {
+            return res.status(200).json(dbDataAfterInsert)
+          }
+        }
+      }
     } catch(err) {
       return next(err)
     }
-
-    this.#checkIfInDb
-    if(!this.dbHasMovie) {
-      throw Error("Couldn't add movie to database")
-    }
-    return res.status(200).json(this.movieData)
   }
 
   async search(req, res, next) {
@@ -105,7 +134,6 @@ class MovieController {
         this.options
       )
       const data = await response.json()
-      console.log(data)
       return res.status(200).json(data)
     } catch(err) {
       return next(err)
--- frontend/src/pages/reviews/WriteReview.js
+++ frontend/src/pages/reviews/WriteReview.js
@@ -1,43 +1,113 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './WriteReview.css'
 
-const WriteReview = () => {
-  const [query, setQuery] = useState("")
+const apiUrl = "http://localhost:3001/api/movies/"
+
+const WriteReview = ({tmdbId}) => {
   const [id, setId] = useState(-1)
-  const [movies, setMovies] = useState([])
+  const [view, setView] = useState("")
+  const [movie, setMovie] = useState({})
 
-  const handleChange = (event) => {
-    setQuery(event.target.value)
-  }
+  useEffect(() => {
+    if(!tmdbId) {
+      setView("selectMovie")
+    } else {
+      setId(tmdbId)
+      setView("writeReview")
+    }
+  }, [tmdbId])
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    axios
-      .get("http://localhost:3001/api/movies/search/"+encodeURI(query))
-      .then((response) => {
-        setMovies(response.data.results)
-      })
-  }
+  const Search = () => {
+    const [query, setQuery] = useState("")
+    const [movies, setMovies] = useState([])
 
-  return(
-    <div className="container">
-      <h1>Valitse elokuva, josta haluat kirjoittaa arvostelun</h1>
-      <div id="search">
-        <form onSubmit={ handleSubmit }>
-          <input type="text" value={ query } onChange={ handleChange } />
-          <button type="submit">Hae</button>
-        </form>
-        <div id="results">
-          <ul>
-            { movies.map((movie) => (
-              <li key={ movie.id }>{ movie.title }</li>
-            ))}
-          </ul>
+    const onText = (event) => {
+      setQuery(event.target.value)
+    }
+
+    const onSearch = (event) => {
+      event.preventDefault()
+      axios
+        .get(apiUrl+"search/"+encodeURI(query))
+        .then((response) => {
+          const firstMovies =  response.data.results.slice(0, 5)
+          setMovies(firstMovies)
+        })
+    }
+
+    const onChoose = (id) => {
+      setId(id)
+      axios
+        .get(apiUrl+id)
+        .then((response) => {
+          setMovie(response.data)
+        })
+      setView("writeReview")
+    }
+    
+    return(
+      <div className="container">
+        <h1>Valitse elokuva, josta haluat kirjoittaa arvostelun</h1>
+        <div id="search">
+          <form onSubmit={onSearch}>
+            <input type="text" value={query} onChange={onText} />
+            <button type="submit">Hae</button>
+          </form>
+          <div id="results">
+            <ul>
+              {movies.map((movie) => (
+                <li key={movie.id}>
+                  <div className="movie">
+                    <img
+                      alt="Kuva elokuvan julisteesta."
+                      src={process.env.REACT_APP_IMG_BASE_URL+movie.poster_path}
+                    />
+                    <h3 className="title">{ movie.title }</h3>
+                    <h3 className="release_year">
+                      {movie.release_date.slice(0, 4)}
+                    </h3>
+                  </div>
+                  <button onClick={() => onChoose(movie.id)}>Arvostele tämä</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
+
+  const Writer = () => {
+    return(
+      <div className="container">
+        <h1>Kirjoita arvostelu</h1>
+        <div id="review-view">
+          <div className="movie">
+            <img
+              alt="Kuva elokuvan julisteesta."
+              src={movie.poster_url}
+            />
+            <h3 className="title">{ movie.title }</h3>
+            <h3 className="release_year">{movie.release_year}</h3>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  const Views = () => {
+    if(view === "selectMovie") {
+      return(<Search />)
+    } else if(view === "writeReview") {
+      return(<Writer />)
+    } else {
+      console.log(id+" "+view)
+      return(<h1>a</h1>)
+    }
+  }
+
+  return(<Views />)
 }
 
 export default WriteReview
