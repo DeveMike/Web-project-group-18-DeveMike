af45b72cc80b4adba920dfe2ce51dc8239e31fa5
DeveMike <roulinski@hotmail.com>
2025-09-30 18:39:33 +0300

    test(auth): add register user test

---
+++ backend/test/auth.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai')
+
+describe('Auth API tests', function () {
+  const apiUrl = 'http://localhost:3001/api/auth/'
+
+  let token = ''
+  const email = `test_${Date.now()}@e.com`
+  const password = 'Salasana1'
+
+  function postOptions(body) {
+    return {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(body)
+    }
+  }
+
+  it('should register a new user', async function () {
+    const res = await fetch(apiUrl + 'register/', postOptions({ email, password }))
+    expect(res.status).to.equal(201)
+    const data = await res.json()
+    expect(data).to.have.property('token')
+    token = data.token
+  })
+
+})
